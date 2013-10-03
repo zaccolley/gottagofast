796fd94c40b1b2de2b2693199b8edbbe4e729344
Zac Colley <hi@zaccolley.com>
2013-10-03 23:34:20 +0100

    A LOAD OF :SHIT:
    Added some ideas for how the games should highlight
    Added a info section and events stuff
    Changed colours and other junk

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,13 @@
 	setTimeout(function(){
 		$('.logo').removeClass('logo-animation');
 	}, 250);
+
+
+	$('li.game').click(function(){
+		$('li.game').each(function(){
+			$(this).addClass('game-unselected');
+		})
+		$(this).removeClass('game-unselected');
+	});
+
 })()
