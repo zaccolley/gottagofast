6ccf12735f7ada315f2eaa5bf169543e579278dd
Zac Colley <hi@zaccolley.com>
2014-03-17 23:17:10 +0000

    First incarnation of account panel

--- routes/account.js
+++ routes/account.js
@@ -4,5 +4,16 @@
  */
 
 exports.show = function(req, res){
-  res.render('account', { title: 'Account ~ UPGS - University of Portsmouth Gaming Society', user: 'Tim' });
+	var data = {};
+
+	data.title = 'Account ~ UPGS - University of Portsmouth Gaming Society';
+
+	data.user = {
+		forename: 'Bill',
+		surname: 'Murray',
+		username: 'muzza',
+		bio: 'i do films and things sometimes'
+	}
+
+	res.render('account', data);
 };
