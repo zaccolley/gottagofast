d6233b06fd719438b8a6a549727b215f311d3be3
Zac Colley <hi@zaccolley.com>
2014-03-17 23:21:33 +0000

    Added a temp Tim user info for demo purposes

--- routes/account.js
+++ routes/account.js
@@ -6,14 +6,23 @@
 exports.show = function(req, res){
 	var data = {};
 
-	data.title = 'Account ~ UPGS - University of Portsmouth Gaming Society';
-
-	data.user = {
+	var bill = {
 		forename: 'Bill',
 		surname: 'Murray',
 		username: 'muzza',
 		bio: 'i do films and things sometimes'
 	}
+
+	var tim = {
+		forename: 'Tim',
+		surname: 'Varley',
+		username: 'digitiltry',
+		bio: 'i am the society pres and do other web tech things'
+	}
+
+	data.user = bill;
+
+	data.title = data.user.forename + '\'s Account ~ UPGS - University of Portsmouth Gaming Society';
 
 	res.render('account', data);
 };
