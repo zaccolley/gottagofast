a6260625ea4edcadaa161501607538b54158d47d
Zac Colley <hi@zaccolley.com>
2014-04-17 15:11:42 +0100

    Swapped login buttons around

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,22 @@ exports.show = function(req, res){
 	db.connection.query('SELECT * FROM game LIMIT 6', function(err, rows){
 		//console.log(rows);
 		data.games = rows;
+
 		if(req.session.user){
+			console.log(req.session.user.user_name);
+			var user = {
+				forename: req.session.user.user_name.split(" ")[0],
+				surname: req.session.user.user_name.split(" ")[1],
+				username: req.session.user.user_handle,
+				bio: req.session.user.user_bio
+			}
+
 			data.loggedin = true;
+			data.user = user;
 		}
+
 		data.title = 'UPGS - University of Portsmouth Gaming Society';
+
 	  	res.render('home', data);
 	});
 };
